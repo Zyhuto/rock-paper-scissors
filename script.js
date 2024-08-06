7d34672e521a8c237bca773fcc8240b2196121d8
Zyhuto <119459963+Zyhuto@users.noreply.github.com>
2024-08-07 01:31:02 +0200

    Add simple UI / change Logic to use UI

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 console.log("Hello World");
 
-
-
-
-
 function getComputerChoice () {
-    switch (Math.floor(Math.random()*3)) {
+    switch (Math.floor(Math.random()*3+1)) {
         case 1:
             return "rock";
         case 2:
             return "paper";
             
         case 3:
-            return "scissor"
+            return "scissors"
     }
-
 }
 
 function getHumanChoice () {
@@ -26,11 +21,7 @@ function getHumanChoice () {
         alert("wrong Input")
         return getHumanChoice();
     }
-
 }
-
-let humanScore = 0;
-let computerScore = 0;
 
 function playRound (humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
@@ -38,14 +29,76 @@ function playRound (humanChoice, computerChoice) {
     } else {
         switch (humanChoice) {
             case "rock":
-                return computerChoice = "paper" ? "computer" : "human";
+                return computerChoice == "paper" ? "computer" : "human";
             case "paper":
-                return computerChoice = "rock" ? "human" : "computer";
-            case "scissor":
-                return computerChoice = "paper" ? "human" : "computer";
+                return computerChoice == "rock" ? "human" : "computer";
+            case "scissors":
+                return computerChoice == "paper" ? "human" : "computer";
         }
     }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+const roundResultText = document.querySelector(".round-result");
+const humanScoreText = document.querySelector(".player-score");
+const computerScoreText = document.querySelector(".computer-score");
+
+const rpsButtons = document.querySelectorAll(".button-container > button");
+rpsButtons.forEach(x => x.addEventListener("click", e => 
+   processClick(e.target.id)
+
+));
+
+function processClick (id) {
+    let roundResult = playRound(id, getComputerChoice());
+    switch (roundResult){
+        case "draw":
+        roundResultText.textContent = "Draw!"
+        break;
+        case "human":
+            roundResultText.textContent = "Human Won!";
+            humanScore++;
+            break;
+        case "computer":
+            roundResultText.textContent = "Computer Won!";
+            computerScore++;
+            break;
+        }
+    humanScoreText.textContent = humanScore;
+    computerScoreText.textContent = computerScore;
+
+    if ( computerScore > 4) {
+        alert("Computer won the Game!")
+        location.reload();
+    }
+    if ( humanScore > 4) {
+        alert("Human won the Game!")
+        location.reload();
+    }
+        
+
+        
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function playGame (rounds) {
     let humanScore = 0;
@@ -76,4 +129,4 @@ function playGame (rounds) {
     }
 } 
 
-playGame(5);
+//playGame(5);
